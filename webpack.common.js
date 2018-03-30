921b3e742412ab58140ff88f0d93b6fd7ee023c9
Radames Ajna <radamajna@gmail.com>
2018-03-30 11:38:40 -0700

    all config on .babelrc

--- webpack.common.js
+++ webpack.common.js
@@ -27,12 +27,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: [/node_modules/],
-        use: [{
-          loader: "babel-loader",
-          options: {
-            presets: ["es2015"]
-          }
-        }]
+        loader: "babel-loader"
       }
     ]
   },
